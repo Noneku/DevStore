f49c8454942229c2e26e4c773ecb6d2ca72bd733
None.ku <dev.nassim.pro@gmail.com>
2023-08-17 16:15:13 +0200

    Add tailwind to the project

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,31 +1,72 @@
-import React from 'react'
-import { useFormik } from 'formik'
+import React from "react";
+import { useFormik } from "formik";
+import {
+  Card,
+  Input,
+  Checkbox,
+  Button,
+  Typography,
+} from "@material-tailwind/react";
 
 const SignUpForm = () => {
-    // Pass the useFormik() hook initial form values and a submit function that will
-    // be called when the form is submitted
-    const formik = useFormik({
-      initialValues: {
-        email: '',
-      },
-      onSubmit: values => {
-        alert(JSON.stringify(values, null, 2));
-      },
-    });
-    return (
-      <form onSubmit={formik.handleSubmit}>
-        <label htmlFor="email">Email Address</label>
-        <input
-          id="email"
-          name="email"
-          type="email"
-          onChange={formik.handleChange}
-          value={formik.values.email}
+  // Pass the useFormik() hook initial form values and a submit function that will
+  // be called when the form is submitted
+  const formik = useFormik({
+    initialValues: {
+      nom: "",
+      prenom: "",
+      username: "",
+      password: "",
+      email: "",
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+  return (
+    <Card color="transparent" shadow={false}>
+      <Typography variant="h4" color="blue-gray">
+        Sign Up
+      </Typography>
+      <Typography color="gray" className="mt-1 font-normal">
+        Enter your details to register.
+      </Typography>
+      <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+        <div className="mb-4 flex flex-col gap-6">
+          <Input size="lg" label="Name" />
+          <Input size="lg" label="Email" />
+          <Input type="password" size="lg" label="Password" />
+        </div>
+        <Checkbox
+          label={
+            <Typography
+              variant="small"
+              color="gray"
+              className="flex items-center font-normal"
+            >
+              I agree the
+              <a
+                href="#"
+                className="font-medium transition-colors hover:text-gray-900"
+              >
+                &nbsp;Terms and Conditions
+              </a>
+            </Typography>
+          }
+          containerProps={{ className: "-ml-2.5" }}
         />
-  
-        <button type="submit">Submit</button>
+        <Button className="mt-6" fullWidth>
+          Register
+        </Button>
+        <Typography color="gray" className="mt-4 text-center font-normal">
+          Already have an account?{" "}
+          <a href="#" className="font-medium text-gray-900">
+            Sign In
+          </a>
+        </Typography>
       </form>
-    );
-  };
+    </Card>
+  );
+};
 
 export default SignUpForm;

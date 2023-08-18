577667cef55cd8df86aeb1986120b4a62ace786d
Noneku <dev.nassim.pro@gmail.com>
2023-08-18 16:10:58 +0200

    Resolve problem display error message

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { useFormik } from "formik";
+import { ErrorMessage, Field, Formik } from "formik";
+import * as Yup from 'yup';
 import {
   Card,
   Input,
@@ -9,20 +10,20 @@ import {
 } from "@material-tailwind/react";
 
 const SignUpForm = () => {
-  // Pass the useFormik() hook initial form values and a submit function that will
-  // be called when the form is submitted
-  const formik = useFormik({
-    initialValues: {
-      nom: "",
-      prenom: "",
-      username: "",
-      password: "",
-      email: "",
-    },
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
+
+  const handleSubmit = ({setSubmitting}) => {
+
+    setSubmitting(false);
+  }
+
+  const validationSchema = Yup.object().shape(
+    {
+      nom: Yup.string()
+      .min(4, "Please enter a name more than 1 character")
+      .required('Le nom est requis')
+    })
+  
+
   return (
     <Card color="transparent" shadow={false}>
       <Typography variant="h4" color="blue-gray">
@@ -31,42 +32,58 @@ const SignUpForm = () => {
       <Typography color="gray" className="mt-1 font-normal">
         Enter your details to register.
       </Typography>
-      <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
-        <div className="mb-4 flex flex-col gap-6">
-          <Input size="lg" label="Name" />
-          <Input size="lg" label="Email" />
-          <Input type="password" size="lg" label="Password" />
-        </div>
-        <Checkbox
-          label={
-            <Typography
-              variant="small"
-              color="gray"
-              className="flex items-center font-normal"
-            >
-              I agree the
-              <a
-                href="#"
-                className="font-medium transition-colors hover:text-gray-900"
-              >
-                &nbsp;Terms and Conditions
-              </a>
-            </Typography>
+      <Formik
+        initialValues={
+          {
+            nom: '',
           }
-          containerProps={{ className: "-ml-2.5" }}
-        />
-        <Button className="mt-6" fullWidth>
-          Register
-        </Button>
-        <Typography color="gray" className="mt-4 text-center font-normal">
-          Already have an account?{" "}
-          <a href="#" className="font-medium text-gray-900">
-            Sign In
-          </a>
-        </Typography>
-      </form>
+        }
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+
+      >  
+        {() => (
+          <form onSubmit={ handleSubmit } className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+          <div className="mb-4 flex flex-col gap-6">
+            <Field component={Input} name="nom" size="lg" label="Nom" className="input" />
+            <ErrorMessage component="span" name="nom" className="text-red-500"/>
+            <Field component={Input} size="lg" label="Prénom" />
+            <Field component={Input} size="lg" label="Pseudo Utilisateur" />
+            <Field component={Input} size="lg" label="Email" />
+            <Field component={Input} type="password" size="lg" label="Password" />
+          </div>
+          <Checkbox
+            label={
+              <Typography
+                variant="small"
+                color="gray"
+                className="flex items-center font-normal"
+              >
+                I agree the
+                <a
+                  href="#"
+                  className="font-medium transition-colors hover:text-gray-900"
+                >
+                  &nbsp;Terms and Conditions
+                </a>
+              </Typography>
+            }
+            containerProps={{ className: "-ml-2.5" }}
+          />
+          <Button type= "submit" className="mt-6" fullWidth>
+            Register
+          </Button>
+          <Typography color="gray" className="mt-4 text-center font-normal">
+            Already have an account?{" "}
+            <a href="#" className="font-medium text-gray-900">
+              Sign In
+            </a>
+          </Typography>
+        </form>
+        )
+      }
+      </Formik>     
     </Card>
-  );
-};
+)};
 
 export default SignUpForm;

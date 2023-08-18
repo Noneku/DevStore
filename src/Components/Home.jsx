a44eadabf81ef2c31fa050a80f41a1b3dc3a5be0
nicodew2 <nicolas.deweireld@gmail.com>
2023-08-18 15:51:25 +0200

    home

---
+++ src/Components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import axios from "axios";
+export function Home() {
+  return (
+    <section className="Home">
+
+        <h2>Catégories</h2>
+
+
+    </section>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,13 @@ import App from './App.jsx'
 import './index.css'
 import { Footer } from './components/Footer.jsx'
 import Header from './Components/Header.jsx'
+import { Home } from './components/Home.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
  <Header /> 
- 
-  <Footer />
+ <Home />
+<Footer />
   </React.StrictMode>,
 )

86e052cdd023a2b44077e67cd216aa7dd286a579
nicodew2 <nicolas.deweireld@gmail.com>
2023-08-18 10:05:30 +0200

    Footer et Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,62 +1,128 @@
-import React from 'react'
-// import {Link} from 'react-router-dom'
-// const Navbar = () => {
-//   return (
-//     <nav className="nav">
-//             <Link className="link" to="/">Accueil</Link>
-//             <label htmlFor="seacrh" >rechercher</label>
-//             <input type="button" value="" />
-            
-        
-//     </nav>
-//   )
-// }
+import React from "react";
 import {
-    Navbar,
-    Typography,
-    IconButton,
-    Button,
-    Input,
-  } from "@material-tailwind/react";
-  import { BellIcon, Cog6ToothIcon } from "@heroicons/react/24/solid";
-   
- const Header=()=> {
-    return (
-      <Navbar className="mx-auto max-w-screen-xl px-4 py-3">
-      <div className="flex flex-wrap items-center justify-between gap-y-4 text-blue-gray-900">
+  Navbar,
+  MobileNav,
+  Typography,
+  Button,
+  IconButton,
+} from "@material-tailwind/react";
+ 
+const Header= () => {
+  const [openNav, setOpenNav] = React.useState(false);
+ 
+  React.useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 960 && setOpenNav(false),
+    );
+  }, []);
+ 
+  const navList = (
+    <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center">
+          Pages
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center">
+          Account
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center">
+          Blocks
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center">
+          Docs
+        </a>
+      </Typography>
+    </ul>
+  );
+ 
+  return (
+    <Navbar className="mx-auto max-w-screen-xl py-2 px-4 lg:px-8 lg:py-4">
+      <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
         <Typography
           as="a"
           href="#"
-          variant="h6"
-          className="mr-4 ml-2 cursor-pointer py-1.5"
+          className="mr-4 cursor-pointer py-1.5 font-medium"
         >
           Material Tailwind
         </Typography>
-        <div className="ml-auto flex gap-1 md:mr-4">
-          <IconButton variant="text" color="blue-gray">
-            <Cog6ToothIcon className="h-4 w-4" />
-          </IconButton>
-          <IconButton variant="text" color="blue-gray">
-            <BellIcon className="h-4 w-4" />
-          </IconButton>
-        </div>
-        <div className="relative flex w-full gap-2 md:w-max">
-          <Input
-            type="search"
-            label="Type here..."
-            className="pr-20"
-            containerProps={{
-              className: "min-w-[288px]",
-            }}
-          />
-          <Button size="sm" className="!absolute right-1 top-1 rounded">
-            Search
+        <div className="hidden lg:block">{navList}</div>
+        <Button variant="gradient" size="sm" className="hidden lg:inline-block">
+          <span>Buy Now</span>
+        </Button>
+        <IconButton
+          variant="text"
+          className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+          ripple={false}
+          onClick={() => setOpenNav(!openNav)}
+        >
+          {openNav ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              className="h-6 w-6"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          )}
+        </IconButton>
+      </div>
+      <MobileNav open={openNav}>
+        <div className="container mx-auto">
+          {navList}
+          <Button variant="gradient" size="sm" fullWidth className="mb-2">
+            <span>Buy Now</span>
           </Button>
         </div>
-      </div>
+      </MobileNav>
     </Navbar>
- 
-    );
-  }
-  export default Header
-
+  );
+}
+export default Header
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {BrowserRouter} from "react-router-dom"
+import { Footer } from './components/Footer.jsx'
+import Header from './Components/Header.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-  <App />
-</BrowserRouter>,
+  <React.StrictMode>
+ <Header /> 
+ 
+  <Footer />
+  </React.StrictMode>,
 )

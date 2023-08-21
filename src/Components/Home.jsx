36d4103fdb1949db8fcb8618f662682e9755db76
59013-96-05 <59013-96-05@exchange.ad.afpanet>
2023-08-21 16:06:17 +0200

    home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,33 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 import axios from "axios";
-export function Home() {
+
+
+const baseURL = 'https://fakestoreapi.com/products';
+export function Home() 
+{
+  const [posts, setPosts] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPosts(response.data);
+    });
+  }, []);
+  console.log(posts)
+  // if (!posts) return null;
+
   return (
-    <section className="Home">
+    <div>
+      
+  {posts
+  
+  .map((post)=><li>{post.category}</li>)
 
-        <h2>Catégories</h2>
-
-
-    </section>
+  
+  
+  
+  
+  
+  }
+    </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Footer } from './components/Footer.jsx'
+import { Footer } from './Components/Footer.jsx'
 import Header from './Components/Header.jsx'
-import { Home } from './components/Home.jsx'
+import { Home } from './Components/Home.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
- <Header /> 
- <Home />
-<Footer />
+    <Header /> 
+    <Home />
+    <Footer />
   </React.StrictMode>,
 )

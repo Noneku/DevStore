d88ca5bf14cfb04ba6f70c041e8113f3e3259179
Noneku <dev.nassim.pro@gmail.com>
2023-08-22 11:06:06 +0200

    Add rules work to Form

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -9,35 +9,43 @@ import {
   Typography,
 } from "@material-tailwind/react";
 
+const validationSchema = Yup.object().shape({
+  nom: Yup.string()
+    .min(4, "Veuillez entrer un nom contenant plus de 4 caractères")
+    .required("Le nom est requis"),
+
+  prenom: Yup.string()
+    .min(1, "Veuillez entrer un prénom contenant plus de 1 caractère")
+    .required("Le prénom est requis"),
+
+  pseudo: Yup.string()
+    .min(1, "Veuillez entrer un pseudo contenant plus de 1 caractère")
+    .required("Le pseudo est requis"),
+
+  email: Yup.string()
+    .email("Email invalide")
+    .required("L'email est requis"),
+
+  password: Yup.string()
+    .min(5, "Veuillez entrer un mot de passe contenant plus de 5 caractères")
+    .required("Le mot de passe est requis"),
+});
+
+
+const initialValues = {
+  nom: '',
+  prenom: '',
+  pseudo: '',
+  email: '',
+  password: '',
+  agreeTerms: false
+};
+
 const SignUpForm = () => {
 
-  const handleSubmit = (values, { setSubmitting }) => {
-    
-    setSubmitting(false);
+  const handleSubmit = () => {
+    alert('je fonctionne !');
   }
-
-  const validationSchema = Yup.object().shape(
-    {
-      nom: Yup.string()
-      .min(4, "Veuillez entrer un nom contenant plus de 1 caractère")
-      .required('Le nom est requis'),
-
-      prenom: Yup.string()
-      .min(4, "Veuillez entrer un prenom contenant plus de 1 caractère")
-      .required('Le prenom est requis'),
-
-      pseudo: Yup.string()
-      .min(4, "Veuillez entrer un pseudo plus de 1 caractère")
-      .required('Le pseudo est requis'),
-
-      email: Yup.string()
-      .min(4, "Veuillez entrer un email plus de 1 caractère")
-      .required('Adresse mail requise'),
-
-      password: Yup.string()
-      .min(4, "Veuillez entrer un mot de passe contenant plus de 1 caractère")
-      .required('Le mot de passe est requis'),
-    })
 
   return (
     <Card color="transparent" shadow={false}>
@@ -48,31 +56,47 @@ const SignUpForm = () => {
         Entrez vos détails pour vous inscrire.
       </Typography>
       <Formik
-        initialValues={
-          {
-            nom: '',
-            prenom: '',
-            pseudo: '',
-            email: '',
-            password: ''
-          }
-        }
+        initialValues={initialValues}
         validationSchema={validationSchema}
         onSubmit={handleSubmit}
       >
         {({ isSubmitting }) => (
           <Form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
             <div className="mb-4 flex flex-col gap-6">
-              <Field component={Input} name="nom" size="lg" label="Nom" className="input" />
-                <ErrorMessage name="nom" component="div" className="text-red-500" />
-              <Field component={Input} size="lg" label="Prénom" name="prenom" />
-                <ErrorMessage name="prenom" component="div" className="text-red-500" />
-              <Field component={Input} size="lg" label="Pseudo Utilisateur" name="pseudo" />
-                <ErrorMessage name="prenom" component="div" className="text-red-500" />
-              <Field component={Input} size="lg" label="Email" name="email" />
-                <ErrorMessage name="email" component="div" className="text-red-500" />
-              <Field component={Input} type="password" size="lg" label="Mot de passe" name="password" />
-                <ErrorMessage name="password" component="div" className="text-red-500" />
+              <Field type="text" name="nom">
+                {({ field }) => (
+                  <Input {...field} size="lg" label="Nom" className="input" />
+                )}
+              </Field>
+              <ErrorMessage name="nom" component="div" className="text-red-500" />
+
+              <Field type="text" name="prenom">
+              {({ field }) => (
+                  <Input {...field} size="lg" label="Prenom" className="input" />
+                )}
+                </Field>
+              <ErrorMessage name="prenom" component="div" className="text-red-500" />
+
+              <Field type="text" name="pseudo">
+              {({ field }) => (
+                  <Input {...field} size="lg" label="Pseudo" className="input" />
+                )}
+                </Field>
+              <ErrorMessage name="pseudo" component="div" className="text-red-500" />
+
+              <Field type="text" name="email">
+              {({ field }) => (
+                  <Input {...field} type="email" size="lg" label="Adresse E-mail" className="input" />
+                )}
+              </Field>
+              <ErrorMessage name="email" component="div" className="text-red-500" />
+
+              <Field type="text" name="password">
+              {({ field }) => (
+                  <Input {...field} type="password" size="lg" label="Mot de passe" className="input" />
+                )}
+              </Field>
+              <ErrorMessage name="password" component="div" className="text-red-500" />
             </div>
             <div className="mb-4">
               <Checkbox
@@ -94,6 +118,7 @@ const SignUpForm = () => {
                 containerProps={{ className: "-ml-2.5" }}
                 name="agreeTerms"
               />
+              <ErrorMessage name="agreeTerms" component="div" className="text-red-500" />
             </div>
             <Button type="submit" className="mt-6" fullWidth disabled={isSubmitting}>
               S'inscrire

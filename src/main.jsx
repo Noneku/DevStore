600c42c832c1b28005626d2bd5663e1e04c157df
None.ku <dev.nassim.pro@gmail.com>
2023-08-23 12:55:34 +0200

    resolve conflict

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import SignUpForm from './components/SignUpForm.jsx'
+import { Footer } from './components/Footer.jsx'
+import {Header} from './components/Header.jsx'
+import { Home } from './components/Home.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Header /> 
+    <Home />
     <SignUpForm />
+    <Footer />
   </React.StrictMode>,
 )

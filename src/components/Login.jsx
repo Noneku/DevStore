0d9859a198da10719505822e31fcfad127e6bd37
Fabien Girard <fabiengirard0410@gmail.com>
2023-08-23 15:23:25 +0200

    update

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,45 +1,93 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
+import * as Yup from 'yup';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
 import axios from 'axios';
 
-function Login() {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [errorMessage, setErrorMessage] = useState("");
+export const loginSchema = Yup.object().shape({
+    username: Yup.string()
+      .required('Username is required'),
+    password: Yup.string()
+      .min(5, 'Password is too short - should be 5 chars minimum.')
+      .required('Password is required'),
+  });
 
-    function handleLogin() {
-        axios.get(`https://fakestoreapi.com/users`).then(response => {
-            const user = response.data.find(user => user.username === username && user.password === password);
-
-            if (user) {
-                console.log("Utilisateur connecté avec succès");
-            } else {
-                setErrorMessage("Identifiant ou mot de passe incorrect");
-            }
-        }).catch(error => {
-            console.error("Erreur lors de la récupération des utilisateurs:", error);
-        });
+  export const Login = ({styles ={
+    label: 'text-red-500', 
+    field: 'border p-2', 
+    errorMsg: 'text-sm text-red-400', 
+    button: 'bg-blue-500 p-2 rounded',
+    forgotPassword: 'text-blue-500'}
+  }) => {
+    const handleSubmit = async (values) => {
+      try {
+        const response = await axios.post('https://fakestoreapi.com/auth/login', values);
+        if(response.data.status === 'success') {
+          alert('Logged in successfully');
+          // Ici, vous pouvez rediriger l'utilisateur ou stocker le token dans le localStorage.
+        }
+      } catch (error) {
+        alert('Login failed. Please check your credentials.');
+      }
+    };
+  
+    const handleForgotPassword = async (username) => {
+        if (!username) {
+          alert("Please enter your username first.");
+          return;
+        }
+        try {
+          await axios.put('https://fakestoreapi.com/users/1', { username: username });
+          alert('Password reset request sent. Check your email.');
+        } catch (error) {
+          alert('Failed to send reset request. Please try again later.');
+        }
+      };
+    
+      return (
+        <Formik
+          initialValues={{
+            username: '',
+            password: '',
+          }}
+          validationSchema={loginSchema}
+          onSubmit={handleSubmit}
+        >
+          {({ values }) => (
+            <Form>
+              <label className={styles.label} htmlFor='username'>
+                Username
+              </label>
+              <Field className={styles.field} id='username' name='username' />
+              <ErrorMessage component='a' className={styles.errorMsg} name='username' />
+    
+              <label className={styles.label} htmlFor='password'>
+                Password
+              </label>
+              <Field className={styles.field} id='password' name='password' type="password" />
+              <ErrorMessage component='a' className={styles.errorMsg} name='password' />
+    
+              <div className='mt-8'>
+                <button type='submit' className={styles.button}>
+                  Login
+                </button>
+              </div>
+    
+              {/* Lien pour la réinitialisation du mot de passe */}
+              <div className="mt-4">
+                <button 
+                  type="button" 
+                  className={styles.forgotPassword}
+                  onClick={() => handleForgotPassword(values.username)}
+                >
+                  Forgot Password?
+                </button>
+              </div>
+    
+            </Form>
+          )}
+        </Formik>
+      );
     }
+ 
 
-    function handleForgotPassword() {
-        console.log(`Demande de réinitialisation du mot de passe pour ${username}`);
-    }
-
-    return (
-        <div>
-            <div class="form-group">
-                <label for="exampleInputemail">E-mail</label>
-                <input type="email"
-                class="form-control"/>
-            </div>
-
-            <div class="form-group">
-                <label for="exampleInputPassword1">Mot de passe</label>
-                <input type="password" class="form-control"/>
-                    </div>
-            <button onClick={handleLogin}>Se connecter</button>
-            <button onClick={handleForgotPassword}>Mot de passe oublié</button>
-        </div>
-    );
-}
-
-export default Login;
+  export default Login;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import Login from './components/Login.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+    <Login />
+  </BrowserRouter>
 )

5b07d7e694d656db3dab9e662b883dd91b6f0707
None.ku <dev.nassim.pro@gmail.com>
2023-08-23 13:11:00 +0200

    Resolve problem property

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {
   Navbar,
-  MobileNav,
+  Collapse,
   Typography,
   Button,
   Menu,
@@ -268,9 +268,9 @@ export function Header() {
         <ProfileMenu />
       </div>
      
-      <MobileNav open={isNavOpen} className="overflow-scroll">
+      <Collapse open={isNavOpen} className="overflow-scroll">
         <NavList />
-      </MobileNav>
+      </Collapse>
     </Navbar>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header /> 
     <Home />
-    <SignUpForm />
+    {/* <SignUpForm /> */}
     <Footer />
   </React.StrictMode>,
 )

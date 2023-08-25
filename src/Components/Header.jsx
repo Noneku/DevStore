744350e9dea1b9efb3064440a31f5bbfc68f450b
Noneku <dev.nassim.pro@gmail.com>
2023-08-25 13:40:19 +0200

    Before merge

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,297 +1,58 @@
-import React from "react";
-import { useState, useEffect} from "react";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
-import {
-  Navbar,
-  Collapse,
-  Typography,
-  Button,
-  Menu,
-  MenuHandler,
-  MenuList,
-  MenuItem,
-  Avatar,
-  Card,
-  IconButton,
-  Input,
-} from "@material-tailwind/react";
-import {
-  CubeTransparentIcon,
-  UserCircleIcon,
-  CodeBracketSquareIcon,
-  Square3Stack3DIcon,
-  ChevronDownIcon,
-  Cog6ToothIcon,
-  InboxArrowDownIcon,
-  LifebuoyIcon,
-  PowerIcon,
-  RocketLaunchIcon,
-  Bars2Icon,
-} from "@heroicons/react/24/outline";
-
+import { Navbar, Collapse, Typography, Button, Menu, MenuHandler, MenuList, MenuItem, Avatar, Card, IconButton, Input } from "@material-tailwind/react";
+import { ChevronDownIcon, RocketLaunchIcon, Bars2Icon } from "@heroicons/react/24/outline";
 import jwtDecode from "jwt-decode";
+import axios from "axios";
 
-// profile menu component
-const profileMenuItems = [
-  {
-    label: "My Profile",
-    icon: UserCircleIcon,
-  },
-  {
-    label: "Edit Profile",
-    icon: Cog6ToothIcon,
-  },
-  {
-    label: "Inbox",
-    icon: InboxArrowDownIcon,
-  },
-  {
-    label: "Help",
-    icon: LifebuoyIcon,
-  },
-  {
-    label: "Sign Out",
-    icon: PowerIcon,
-  },
-];
-
-function ProfileMenu() {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-
-  const closeMenu = () => setIsMenuOpen(false);
-
-  return (
-    <Menu open={isMenuOpen} handler={setIsMenuOpen} placement="bottom-end">
-      <MenuHandler>
-        <Button
-          variant="text"
-          color="blue-gray"
-          className="flex items-center gap-1 rounded-full py-0.5 pr-2 pl-0.5 lg:ml-auto"
-        >
-          <Avatar
-            variant="circular"
-            size="sm"
-            alt="tania andrew"
-            className="border border-gray-900 p-0.5"
-            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
-          />
-          <ChevronDownIcon
-            strokeWidth={2.5}
-            className={`h-3 w-3 transition-transform ${
-              isMenuOpen ? "rotate-180" : ""
-            }`}
-          />
-        </Button>
-      </MenuHandler>
-      <MenuList className="p-1">
-        {profileMenuItems.map(({ label, icon }, key) => {
-          const isLastItem = key === profileMenuItems.length - 1;
-          return (
-            <MenuItem
-              key={label}
-              onClick={closeMenu}
-              className={`flex items-center gap-2 rounded ${
-                isLastItem
-                  ? "hover:bg-red-500/10 focus:bg-red-500/10 active:bg-red-500/10"
-                  : ""
-              }`}
-            >
-              {React.createElement(icon, {
-                className: `h-4 w-4 ${isLastItem ? "text-red-500" : ""}`,
-                strokeWidth: 2,
-              })}
-              <Typography
-                as="span"
-                variant="small"
-                className="font-normal"
-                color={isLastItem ? "red" : "inherit"}
-              >
-                {label}
-              </Typography>
-            </MenuItem>
-          );
-        })}
-      </MenuList>
-    </Menu>
-  );
-}
-
-// nav list menu
-const navListMenuItems = [
-  {
-    title: "Se connecter",
-    description:
-      "Je me connecte ici.",
-      route: "/login"
-  },
-  {
-    title: "Inscription",
-    description:
-      "Je m'inscris ici.",
-      route: "/inscription"
-  },
-  {
-    title: "Mon panier.",
-    description:
-      "Je consulte mon panier.",
-      route: "/panier"
-  }
-];
-
-function NavListMenu() {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-
-  const renderItems = navListMenuItems.map(({ title, description, route }) => (
-    <NavLink to={route} key={title}>
-      <MenuItem>
-        <Typography variant="h6" color="blue-gray" className="mb-1">
-          {title}
-        </Typography>
-        <Typography variant="small" color="gray" className="font-normal">
-          {description}
-        </Typography>
-      </MenuItem>
-    </NavLink>
-  ));
-
-  return (
-    <React.Fragment>
-      <Menu allowHover open={isMenuOpen} handler={setIsMenuOpen}>
-        <MenuHandler>
-          <Typography as="a" href="#" variant="small" className="font-normal">
-            <MenuItem className="hidden items-center gap-2 text-blue-gray-900 lg:flex lg:rounded-full">
-              <Square3Stack3DIcon className="h-[18px] w-[18px]" /> Pages{" "}
-              <ChevronDownIcon
-                strokeWidth={2}
-                className={`h-3 w-3 transition-transform ${
-                  isMenuOpen ? "rotate-180" : ""
-                }`}
-              />
-            </MenuItem>
-          </Typography>
-        </MenuHandler>
-        <MenuList className="hidden w-[36rem] grid-cols-7 gap-3 overflow-visible lg:grid">
-          <Card
-            color="blue"
-            shadow={false}
-            variant="gradient"
-            className="col-span-3 grid h-full w-full place-items-center rounded-md"
-          >
-            <RocketLaunchIcon strokeWidth={1} className="h-28 w-28" />
-          </Card>
-          <ul className="col-span-4 flex w-full flex-col gap-1">
-            {renderItems}
-          </ul>
-        </MenuList>
-      </Menu>
-      <MenuItem className="flex items-center gap-2 text-blue-gray-900 lg:hidden">
-        <Square3Stack3DIcon className="h-[18px] w-[18px]" /> Pages{" "}
-      </MenuItem>
-      <ul className="ml-6 flex w-full flex-col gap-1 lg:hidden">
-        {renderItems}
-      </ul>
-    </React.Fragment>
-  );
-}
-
-// nav list component
-const navListItems = [
-  {
-    label: "Account",
-    icon: UserCircleIcon,
-    route: "/profile"
-  }
-];
-
-function NavList() {
-  return (
-    <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center">
-      <NavListMenu />
-      {navListItems.map(({ label, icon, route },index) => (
-        <NavLink to={route} key={index}>
-          <Typography
-          key={label}
-          variant="small"
-          color="blue-gray"
-          className="font-normal"
-        >
-          <MenuItem className="flex items-center gap-2 lg:rounded-full">
-            {React.createElement(icon, { className: "h-[18px] w-[18px]" })}{" "}
-            {label}
-          </MenuItem>
-          </Typography>
-        </NavLink>
-        
-      ))}
-    </ul>
-  );
-}
-// const baseURL = "https://fakestoreapi.com/products/category";
-
+// Composant de la barre de navigation
 export function Header() {
-  const [isNavOpen, setIsNavOpen] = React.useState(false);
-  const [searchValue,setSearchValue] = useState("");
-  const [products,setProducts] = useState([]);
-  const toggleIsNavOpen = () => setIsNavOpen((cur) => !cur);
-
-  const Navigate = useNavigate()
-  // const [posts, setPosts] = useState([]); // État pour stocker les données de l'API
-  // const [loading, setLoading] = useState(true); //État pour gérer le chargement des données
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const [products, setProducts] = useState([]);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  React.useEffect(() => {
-    window.addEventListener(
-      "resize",
-      () => window.innerWidth >= 960 && setIsNavOpen(false)
-    );
-    
-    //TOKEN Traitement
+  const toggleIsNavOpen = () => setIsNavOpen((cur) => !cur);
+
+  useEffect(() => {
+    window.addEventListener("resize", () => window.innerWidth >= 960 && setIsNavOpen(false));
+
+    // Gestion de l'authentification ici
     const token = localStorage.getItem('authToken');
     let decode = jwtDecode(token);
 
-    console.log(decode);
-
-    if(token){
+    if (token) {
       setIsAuthenticated(true);
-
     }
-
   }, []);
 
-  const handleChange = (e) =>{
-    console.log(e.target.value);
-  
+  const handleChange = (e) => {
     setSearchValue(e.target.value);
+  }
 
-  }
-  
   const handleProductClick = (product) => {
-    // Assurez-vous que product.id est une chaîne, car il sera utilisé dans l'URL.
     const productId = String(product.id);
-    Navigate(`/product/${productId}`);
-    setSearchValue("");
+    // Rediriger vers la page du produit
+    // Assurez-vous que vous avez configuré vos routes pour gérer cela.
   }
-  
+
   useEffect(() => {
     if (searchValue !== "") {
-        axios.get('https://fakestoreapi.com/products')
-            .then((response) => {
-                if (response.data) {
-                  setProducts(response.data);
-                  console.log(response.data)
-                  console.log(searchValue);
-                }
-            })
-            .catch((error) => console.log(error));
+      axios.get('https://fakestoreapi.com/products')
+        .then((response) => {
+          if (response.data) {
+            setProducts(response.data);
+          }
+        })
+        .catch((error) => console.log(error));
     } else {
-        setProducts([]);
+      setProducts([]);
     }
   }, [searchValue]);
 
   const filteredCategories = products?.filter((product) =>
-  product.title.toLowerCase().includes(searchValue.toLowerCase()));
+    product.title.toLowerCase().includes(searchValue.toLowerCase())
+  );
 
   return (
     <Navbar className="mx-auto max-w-screen-xl p-2 lg:rounded-full lg:pl-6 bg-orange-500">
@@ -302,10 +63,10 @@ export function Header() {
           </Typography>
         ) : (
           <Typography className="mr-4 ml-2 cursor-pointer py-1.5 font-medium">
-            Je suis deconnecté
+            Je suis déconnecté
           </Typography>
-        )};
-  
+        )}
+        
         <div className="relative flex w-full gap-2 md:w-max">
           <Input
             value={searchValue}
@@ -319,7 +80,7 @@ export function Header() {
           />
           {searchValue !== '' && (
             <ul className="suggestions absolute top-12 bg-gray-100">
-              {filteredCategories.map((product, index) => ( 
+              {filteredCategories.map((product, index) => (
                 <li
                   key={index}
                   onClick={() => handleProductClick(product)}
@@ -332,10 +93,41 @@ export function Header() {
           <Button size="sm" className="absolute right-1 top-1 rounded">
             Search
           </Button>
-        </div> 
-          <NavList />
-        <div className="absolute top-2/4 left-2/4 hidden -translate-x-2/4 -translate-y-2/4 lg:block">
         </div>
+        
+        <ul className="ml-6 flex w-full flex-col gap-1 lg:hidden">
+          <li>
+            <NavLink to="/login">
+              <Typography variant="h6" color="blue-gray" className="mb-1">
+                Se connecter
+              </Typography>
+              <Typography variant="small" color="gray" className="font-normal">
+                Je me connecte ici.
+              </Typography>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/inscription">
+              <Typography variant="h6" color="blue-gray" className="mb-1">
+                Inscription
+              </Typography>
+              <Typography variant="small" color="gray" className="font-normal">
+                Je m'inscris ici.
+              </Typography>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/panier">
+              <Typography variant="h6" color="blue-gray" className="mb-1">
+                Mon panier.
+              </Typography>
+              <Typography variant="small" color="gray" className="font-normal">
+                Je consulte mon panier.
+              </Typography>
+            </NavLink>
+          </li>
+        </ul>
+
         <IconButton
           size="sm"
           color="blue-gray"
@@ -345,12 +137,24 @@ export function Header() {
         >
           <Bars2Icon className="h-6 w-6" />
         </IconButton>
-        <ProfileMenu />
       </div>
-  
+
       <Collapse open={isNavOpen} className="overflow-scroll">
-        <NavList />
+        <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center">
+          <li>
+            <NavLink to="/profile">
+              <Typography variant="small" color="blue-gray" className="font-normal">
+                Account
+              </Typography>
+              <MenuItem className="flex items-center gap-2 lg:rounded-full">
+                <UserCircleIcon className="h-[18px] w-[18px]" /> Account
+              </MenuItem>
+            </NavLink>
+          </li>
+        </ul>
       </Collapse>
     </Navbar>
   );
-              }  
+}
+
+export default Header;

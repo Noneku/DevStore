706567f712e4273f00da741e3c5b44432275c476
Noneku <dev.nassim.pro@gmail.com>
2023-08-27 23:13:48 +0200

    Add package JWT-Decode WORK !

--- src/Components/utils/AuthUser.jsx
+++ src/Components/utils/AuthUser.jsx
@@ -1,20 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import { Typography } from "@material-tailwind/react";
+import jwtDecode from 'jwt-decode';
 
 function AuthUser() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const TokenUser = localStorage.getItem('authToken');
+  let decodedToken = null;
+
+  if(isAuthenticated){
+    const token = TokenUser;
+    decodedToken = jwtDecode(token);
+  }
 
   useEffect(() => { 
     TokenUser && setIsAuthenticated(true);
-
-    console.log(isAuthenticated);
+  
   }, [TokenUser, isAuthenticated]); //On passe des dépendance à useEffect car on lui signal que l'état du composant peut changer
 
   return (
     isAuthenticated ? (
       <Typography className="mr-4 ml-2 cursor-pointer py-1.5 font-medium">
-        Bonjour, <strong className="font-semibold text-gray-900 dark:text-white">tt</strong> 
+        Bonjour, <strong className="font-semibold text-gray-900 dark:text-white">{decodedToken ? decodedToken.user : null}</strong> 
       </Typography>
     ) 
     : null
